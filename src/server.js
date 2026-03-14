2a253c3ae9da3b3e8efde35ced07c3649e5ff8bf
Anna Makarova <makarovaanna07@gmail.com>
2026-03-14 22:09:04 +0200

    feat: update server configuration with helmet for security and add new routes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,59 @@
 // src/server.js
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino-http';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
 });
 
-// Запуск сервера
-app.listen(PORT, () => {
+app.get('/notes/:noteId', (req, res) => {
+  res
+    .status(200)
+    .json({ message: `Retrieved note with ID: ${req.params.noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res
+    .status(500)
+    .json({ message: isProd ? 'Oops something went wrong' : err.stack });
+});
+
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
